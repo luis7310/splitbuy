7962ec7f6a337cc1f7f4211a675a9450f56f3dd4
luis7310 <layon@mundocuervo.com>
2025-06-26 17:30:00 -0600

    [ADD] diseño de navbar

--- splitbuy-front/src/assets/elements/NavBar.jsx
+++ splitbuy-front/src/assets/elements/NavBar.jsx
@@ -8,8 +8,24 @@ export default function NavBar(){
 
     return(
        <nav id='navContainer'>
-            <h1 className='tittle'>SplitBuy</h1>
-            <button onClick={cerrarSesion}>Cerrar sesión</button>
+            <div id="nav-left">
+                <button className='btn-design'>SplitBuy</button>
+                <button className='btn-design'>Inicio</button>
+                <button className='btn-design'>Agregar grupo</button>
+            </div>
+            <div id='nav-center'>
+                <input placeholder='Buscar gasto' id="input-search" type='text'></input>
+                <button className='btn-design'>Buscar</button>
+            </div>
+            <div id="nav-right">
+                <select>
+                    <option disabled selected>Perfil</option>
+                    <option value="ver">Actualizar contraseña</option>
+                    <option value="crear">Configración</option>
+                    <option value="crear" onSelect={cerrarSesion}>Cerrar sesión</option>
+                </select>
+            </div>
+            <button id="menu-btn">=</button>
        </nav>
     )
 }
--- splitbuy-front/src/assets/rutas/NotFound.jsx
+++ splitbuy-front/src/assets/rutas/NotFound.jsx
@@ -1,8 +1,9 @@
-
+import NavBar from "../elements/NavBar"
 
 export default function NotFound(){
     return(
         <>
+        <NavBar></NavBar>
         <h1>404 NOT FOUND</h1>
         </>
     )
