f8e0e29790d2eb9c56c634444069f5eee67200f6
luis7310 <layon@mundocuervo.com>
2025-07-16 19:51:19 -0600

    [FIX] Botones open modal

--- splitbuy-front/src/assets/elements/gastoWindows.jsx
+++ splitbuy-front/src/assets/elements/gastoWindows.jsx
@@ -10,8 +10,8 @@ export default function GastoWindows({idGrupo, nombreGrupo, descripcionGrupo, fe
                 <h1>{nombreGrupo}</h1>
                 <p>{descripcionGrupo}</p>
                 <p>Creado el: {fechaCreado.slice(0, 10)}</p>
-                <button className='btn_window' onClick={() => setAgregarG(true)}>Agregar gasto</button>
-                <button className='btn_window' onClick={() => setAgregarU(true)}>Agregar miembro</button>
+                <button className='btn_window' onClick={() => {setAgregarG(true); setAgregarU(false);}}>Agregar gasto</button>
+                <button className='btn_window' onClick={() => {setAgregarU(true); setAgregarG(false);}}>Agregar miembro</button>
                 <button className='btn_window'>Abandonar grupo</button>
                 {agregarGastoM && (
                     <div className='containerModal' id="AgregarGasto">
